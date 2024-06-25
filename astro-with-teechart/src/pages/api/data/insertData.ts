c251d6395214425df86d61cbcd6bebd6b8abf699
ericramirezhidalgo <ericramirezhidalgo@gmail.com>
2024-06-25 15:52:32 +0200

    added updateData api code + updated setting button + added insert data modal

--- astro-with-teechart/src/pages/api/data/insertData.ts
+++ astro-with-teechart/src/pages/api/data/insertData.ts
@@ -7,13 +7,18 @@ export const POST: APIRoute = async function post({ request }) {
     return new Response('No request body', { status: 400 });
   }
   try {
-    console.log('Request body:', request.body);
     const chartData = await request.json();
     console.log('Parsed chartData:', chartData);
+
+    if (!chartData.id || !chartData.type || !chartData.data) {
+      console.error('Missing required fields');
+      return new Response('Missing required fields', { status: 400 });
+    }
+
     await db.insert(ChartData).values([chartData]);
     return new Response('Chart data inserted', { status: 200 });
   } catch (error) {
     console.error('Error inserting chart data:', error);
     return new Response('Error inserting chart data', { status: 500 });
   }
-}
+};
--- astro-with-teechart/src/pages/api/user/getUser.ts
+++ astro-with-teechart/src/pages/api/user/getUser.ts
@@ -1,26 +1,26 @@
 import { db, User, eq } from 'astro:db';
-import type {APIRoute} from 'astro';
+import type { APIRoute } from 'astro';
 
 export const POST: APIRoute = async function post({ request }) {
-    if (!request.body) {
-      console.log('No request body:', request.body);
-      return new Response('No request body', { status: 400 });
-    }
-    try {
-      console.log('Request body:', request.body);
-      let { username, mail } = await request.json();
-      console.log('Parsed username and mail:', username, mail);
-  
-      const users = await db.select().from(User).where(u => eq(u.username, username) || eq(u.mail, mail));
-      const user = users[0];
-  
-      if (user) {
-        return new Response('User exists', { status: 409 }); // Conflict
-      } else {
-        return new Response('User not found', { status: 200 });
-      }
-    } catch (error) {
-      console.error('Error finding user:', error);
-      return new Response('Error finding user', { status: 500 });
-    }
+  if (!request.body) {
+    console.log('No request body:', request.body);
+    return new Response('No request body', { status: 400 });
   }
+  try {
+    console.log('Request body:', request.body);
+    const { username, mail } = await request.json();
+    console.log('Parsed username and mail:', username, mail);
+
+    const users = await db.select().from(User).where(u => eq(u.username, username) || eq(u.mail, mail));
+    const user = users[0];
+
+    if (user) {
+      return new Response('User exists', { status: 409 });
+    } else {
+      return new Response('User not found', { status: 200 });
+    }
+  } catch (error) {
+    console.error('Error finding user:', error);
+    return new Response('Error finding user', { status: 500 });
+  }
+}
