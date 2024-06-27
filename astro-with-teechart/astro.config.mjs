99a487bd64acc3474423d4d12a94cc9b1349b45a
ericramirezhidalgo <ericramirezhidalgo@gmail.com>
2024-06-27 10:08:08 +0200

    change to astro config

--- astro-with-teechart/astro.config.mjs
+++ astro-with-teechart/astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), db()],
   functions: {
     // Rutas de las funciones de la API
-    api: './src/pages/api',
+    api: './src/pages/api'
   },
-  output: 'server', // o 'hybrid'
+  output: 'static' // o 'hybrid'
+  ,
+  adapter: vercel()
 });
