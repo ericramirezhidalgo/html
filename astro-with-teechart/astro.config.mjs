e24a2cd2dbeabeb71739aeb739b584279add55ca
ericramirezhidalgo <ericramirezhidalgo@gmail.com>
2024-06-28 12:32:43 +0200

    added site to astro.config.mjs

--- astro-with-teechart/astro.config.mjs
+++ astro-with-teechart/astro.config.mjs
@@ -5,6 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-teechart.vercel.app/",
   integrations: [tailwind(), db()],
   functions: {
     // Rutas de las funciones de la API
